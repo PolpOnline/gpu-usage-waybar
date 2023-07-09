46d1636d6cee6c599cc745e5c85462219c8f63da
PolpOnline <git@t0mmy.anonaddy.com>
2023-07-09 19:51:23 +0200

    Make it more elegant (I guess?)

--- src/main.rs
+++ src/main.rs
@@ -2,83 +2,25 @@ use serde::Serialize;
 use std::ops::Add;
 
 fn main() {
+    let gpu_status = NvidiaSmiOutput::new();
+
     let output = OutputFormat {
-        text: get_text(),
-        tooltip: get_tooltip(),
+        text: get_text(&gpu_status),
+        tooltip: get_tooltip(&gpu_status),
     };
 
     println!("{}", serde_json::to_string(&output).unwrap());
 }
 
-fn get_memory_used_percent() -> u32 {
-    let out = &std::process::Command::new("nvidia-smi")
-        .arg("--format=csv,noheader")
-        .arg("--query-gpu=memory.used,memory.total")
-        .output()
-        .unwrap()
-        .stdout;
-    let out = String::from_utf8_lossy(out);
-
-    let arr: Vec<f32> = out
-        .replace(',', "")
-        .replace("MiB", "")
-        .split_whitespace()
-        .map(|x| x.replace("MiB", "").parse::<f32>().unwrap())
-        .collect();
-
-    let mem_used_percent = (arr[0] / arr[1]) * 100.0;
-
-    mem_used_percent.round() as u32
+fn get_text(gpu_status: &NvidiaSmiOutput) -> String {
+    format!(
+        "{}|{}%",
+        gpu_status.gpu_util.clone(),
+        gpu_status.compute_mem_usage()
+    )
 }
 
-fn get_text() -> String {
-    let out = &std::process::Command::new("nvidia-smi")
-        .arg("--format=csv,noheader,nounits")
-        .arg("--query-gpu=utilization.gpu")
-        .output()
-        .unwrap()
-        .stdout;
-    let out = String::from_utf8_lossy(out).trim().parse::<u32>().unwrap();
-
-    let output = TextOutput {
-        gpu_util: out.to_string(),
-        mem_usage: get_memory_used_percent().to_string(),
-    };
-
-    format!("{}%|{}%", output.gpu_util, output.mem_usage)
-}
-
-fn get_tooltip() -> String {
-    let out = &std::process::Command::new("nvidia-smi")
-        .arg("--format=csv,noheader")
-        .arg("--query-gpu=utilization.gpu,utilization.memory,utilization.encoder,utilization.decoder,temperature.gpu,power.draw,pstate,memory.used,memory.total,fan.speed")
-        .output()
-        .unwrap()
-        .stdout;
-    let out = String::from_utf8_lossy(out);
-
-    let out = out.replace(' ', "").replace(',', " ");
-
-    let arr = out.split_whitespace();
-
-    let mut gpu_status = TooltipOutput::default();
-
-    for (i, val) in arr.enumerate() {
-        match i {
-            0 => gpu_status.gpu_util = val.to_string(),
-            1 => gpu_status.mem_util = val.to_string(),
-            2 => gpu_status.enc_util = val.to_string(),
-            3 => gpu_status.dec_util = val.to_string(),
-            4 => gpu_status.temp = val.to_string().add("°C"),
-            5 => gpu_status.power = val.to_string(),
-            6 => gpu_status.pstate = val.to_string(),
-            7 => gpu_status.mem_used = val.to_string().replace("MiB", ""),
-            8 => gpu_status.mem_total = val.to_string(),
-            9 => gpu_status.fan_speed = val.to_string(),
-            _ => (),
-        }
-    }
-
+fn get_tooltip(gpu_status: &NvidiaSmiOutput) -> String {
     format!(
         "GPU: {}\n\
         MEM USED: {}/{} ({}%)\n\
@@ -92,7 +34,7 @@ fn get_tooltip() -> String {
         gpu_status.gpu_util,
         gpu_status.mem_used,
         gpu_status.mem_total,
-        get_memory_used_percent(),
+        gpu_status.compute_mem_usage(),
         gpu_status.mem_util,
         gpu_status.enc_util,
         gpu_status.dec_util,
@@ -104,7 +46,7 @@ fn get_tooltip() -> String {
 }
 
 #[derive(Default)]
-struct TooltipOutput {
+struct NvidiaSmiOutput {
     gpu_util: String,
     mem_util: String,
     enc_util: String,
@@ -117,14 +59,50 @@ struct TooltipOutput {
     fan_speed: String,
 }
 
+impl NvidiaSmiOutput {
+    fn new() -> Self {
+        let out = &std::process::Command::new("nvidia-smi")
+            .arg("--format=csv,noheader")
+            .arg("--query-gpu=utilization.gpu,utilization.memory,utilization.encoder,utilization.decoder,temperature.gpu,power.draw,pstate,memory.used,memory.total,fan.speed")
+            .output()
+            .unwrap()
+            .stdout;
+        let out = String::from_utf8_lossy(out);
+        let out = out.replace(' ', "").replace(',', " ");
+
+        let split = out.split_whitespace();
+
+        let mut gpu_status = NvidiaSmiOutput::default();
+
+        for (i, val) in split.enumerate() {
+            match i {
+                0 => gpu_status.gpu_util = val.to_owned(),
+                1 => gpu_status.mem_util = val.to_owned(),
+                2 => gpu_status.enc_util = val.to_owned(),
+                3 => gpu_status.dec_util = val.to_owned(),
+                4 => gpu_status.temp = val.to_owned().add("°C"),
+                5 => gpu_status.power = val.to_owned(),
+                6 => gpu_status.pstate = val.to_owned(),
+                7 => gpu_status.mem_used = val.to_owned().replace("MiB", ""),
+                8 => gpu_status.mem_total = val.to_owned(),
+                9 => gpu_status.fan_speed = val.to_owned(),
+                _ => (),
+            }
+        }
+
+        gpu_status
+    }
+
+    fn compute_mem_usage(&self) -> u8 {
+        let mem_used_percent = (self.mem_used.parse::<f32>().unwrap()
+            / self.mem_total.replace("MiB", "").parse::<f32>().unwrap())
+            * 100f32;
+        mem_used_percent.round() as u8
+    }
+}
+
 #[derive(Default, Serialize)]
 struct OutputFormat {
     text: String,
     tooltip: String,
-}
-
-#[derive(Default)]
-struct TextOutput {
-    gpu_util: String,
-    mem_usage: String,
 }
