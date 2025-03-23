208099038b1589d55ffe4ca050f2ce16455aed4a
PolpOnline <git@t0mmy.anonaddy.com>
2025-03-23 16:24:01 +0100

    Remove lazy_static as a dep

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ pub mod gpu_status;
 pub mod nvidia;
 
 use clap::Parser;
+use std::sync::OnceLock;
 use std::time::Duration;
 
 use crate::amd::{AmdGpuStatus, AmdSysFS};
 use crate::gpu_status::{GpuStatus, GpuStatusData};
 use crate::nvidia::NvidiaGpuStatus;
 use color_eyre::eyre::{eyre, Result};
-use lazy_static::lazy_static;
 use nvml_wrapper::Nvml;
 use serde::Serialize;
 
@@ -34,8 +34,10 @@ impl Instance {
     }
 }
 
-lazy_static! {
-    pub static ref INSTANCE: Instance = Instance::new().unwrap();
+pub static INSTANCE: OnceLock<Instance> = OnceLock::new();
+
+fn get_instance() -> &'static Instance {
+    INSTANCE.get_or_init(|| Instance::new().unwrap())
 }
 
 #[derive(Parser, Debug)]
@@ -55,7 +57,7 @@ fn main() -> Result<()> {
 
     let args = Args::parse();
 
-    let gpu_status_handler: Box<dyn GpuStatus> = match &*INSTANCE {
+    let gpu_status_handler: Box<dyn GpuStatus> = match get_instance() {
         Instance::Nvml(nvml) => Box::new(NvidiaGpuStatus::new(nvml)?),
         Instance::Amd(amd_sys_fs) => Box::new(AmdGpuStatus::new(amd_sys_fs)?),
     };
