98282ffbb08dc097d15c921e36fb1bd73050f22a
bowen951209 <bowen951209@gmail.com>
2026-01-18 07:41:33 +0800

    Inline get_config_dir()
    This simplifies error handling.

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,24 +1,14 @@
 pub mod structs;
 
-use std::path::PathBuf;
-
 use color_eyre::{Result, eyre::eyre};
 use etcetera::{BaseStrategy, base_strategy::Xdg};
 
 use crate::config::structs::ConfigFile;
 
-fn get_xdg_dir() -> Xdg {
-    Xdg::new().expect("Failed to get XDG directory")
-}
-
-fn get_config_dir() -> PathBuf {
-    get_xdg_dir().config_dir()
-}
-
 const EXAMPLE_CONFIG: &str = include_str!("../../config.example.toml");
 
 pub fn get_or_init_config() -> Result<ConfigFile> {
-    let config_dir = get_config_dir();
+    let config_dir = Xdg::new()?.config_dir();
 
     let config_path = config_dir.join("gpu_usage_waybar.toml");
 
