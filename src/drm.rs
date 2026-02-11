5fd38d525706e741937a31bed5799aa88e6098b8
bowen951209 <bowen951209@gmail.com>
2026-02-11 08:06:06 +0800

    Sort the drm devices returned in scan_drm_devices()
    Sort the drm devices based on their child named `cardX`.

--- src/drm.rs
+++ src/drm.rs
@@ -7,17 +7,19 @@ use color_eyre::eyre::{self, OptionExt};
 /// A [DrmDevice] is the parent of DRM leaf nodes,
 /// usually being `card0` and `renderD128`.
 #[derive(Debug)]
-pub struct DrmDevice(udev::Device);
+pub struct DrmDevice {
+    device: udev::Device,
+    children: Vec<udev::Device>,
+}
 
 impl DrmDevice {
-    fn get_vendor_id_str(&self) -> eyre::Result<&str> {
-        let pci_id = self
-            .0
-            .property_value("PCI_ID")
-            .ok_or_eyre("Cannot find PCI_ID for device")?
-            .to_str()
-            .unwrap();
-        Ok(&pci_id[0..4])
+    pub fn get_dri_card_index(&self) -> Option<u8> {
+        let maybe_index_str = self
+            .children
+            .iter()
+            .find_map(|dev| dev.sysname().to_str().unwrap().strip_prefix("card"));
+
+        maybe_index_str.map(|idx| idx.parse().unwrap())
     }
 
     pub fn get_vendor_name(&self) -> eyre::Result<OsString> {
@@ -28,6 +30,16 @@ impl DrmDevice {
             .ok_or_eyre("No vendor name result exits in database")?;
         Ok(vendor_name.to_owned())
     }
+
+    fn get_vendor_id_str(&self) -> eyre::Result<&str> {
+        let pci_id = self
+            .device
+            .property_value("PCI_ID")
+            .ok_or_eyre("Cannot find PCI_ID for device")?
+            .to_str()
+            .unwrap();
+        Ok(&pci_id[0..4])
+    }
 }
 
 pub fn scan_drm_devices() -> eyre::Result<Vec<DrmDevice>> {
@@ -36,19 +48,30 @@ pub fn scan_drm_devices() -> eyre::Result<Vec<DrmDevice>> {
     enumerator.match_subsystem("drm").unwrap();
     enumerator.match_property("DEVNAME", "/dev/dri/*").unwrap();
 
-    let mut drm_devices = Vec::new();
+    let mut drm_devices: Vec<DrmDevice> = Vec::new();
     for dev in enumerator.scan_devices()? {
         let parent = dev.parent().expect("DRM device must have a parent");
 
-        if !drm_devices
-            .iter()
-            .any(|drm_dev: &DrmDevice| drm_dev.0.sysname() == parent.sysname())
+        if let Some(drm_device) = drm_devices
+            .iter_mut()
+            .find(|drm_dev| drm_dev.device.sysname() == parent.sysname())
         {
-            drm_devices.push(DrmDevice(parent));
+            drm_device.children.push(dev);
+        } else {
+            drm_devices.push(DrmDevice {
+                device: parent,
+                children: vec![dev],
+            });
         }
     }
 
+    sort_by_dri_card_index(&mut drm_devices);
+
     Ok(drm_devices)
+}
+
+fn sort_by_dri_card_index(devs: &mut [DrmDevice]) {
+    devs.sort_by_key(|dev| dev.get_dri_card_index().unwrap_or(u8::MAX));
 }
 
 #[test]
